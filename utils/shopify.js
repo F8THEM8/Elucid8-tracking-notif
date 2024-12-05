651a226122d1bd66cc097e82978e387fcd867e40
F8THEM8 <faithfebisdesign@gmail.com>
2024-12-05 23:25:54 +0100

    Update shopify.js

--- utils/shopify.js
+++ utils/shopify.js
@@ -2,13 +2,15 @@ const fetchOrderData = async (orderNumber, email) => {
   const response = await fetch(
     `https://elucid8-jewelry.com/admin/api/2024-01/orders.json?name=${orderNumber}&email=${email}`,
     {
-      method: "GET",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
         "X-Shopify-Access-Token": process.env.SHOPIFY_ACCESS_TOKEN,
       },
     }
   );
+ const data = await response.json();
+  console.log("Shopify Order Data:", data);  // Log the response for debugging
 
   const { orders } = await response.json();
   return orders.length > 0 ? orders[0] : null;
