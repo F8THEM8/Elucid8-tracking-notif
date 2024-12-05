6a255cb6154f4bcff97e74c3b9f092c043df89da
F8THEM8 <faithfebisdesign@gmail.com>
2024-12-05 16:21:49 +0100

    Create shopify.js

---
+++ utils/shopify.js
@@ -0,0 +1,17 @@
+const fetchOrderData = async (orderNumber, email) => {
+  const response = await fetch(
+    `https://your-shopify-store.myshopify.com/admin/api/2024-01/orders.json?name=${orderNumber}&email=${email}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": process.env.SHOPIFY_ACCESS_TOKEN,
+      },
+    }
+  );
+
+  const { orders } = await response.json();
+  return orders.length > 0 ? orders[0] : null;
+};
+
+export default fetchOrderData;
