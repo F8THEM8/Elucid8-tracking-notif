bf7278bb90c981cedd480141e9de4c9952ca9442
F8THEM8 <faithfebisdesign@gmail.com>
2024-12-06 01:49:33 +0100

    Update get-tracking.js

--- api/get-tracking.js
+++ api/get-tracking.js
@@ -41,7 +41,7 @@ export default async (req, res) => {
       }
 
       const trackingNumber = orderData.tracking_number;
-      const trackingData = await fetchWithTimeoutAndRetry(`https://api.shippo.com/track/${trackingNumber}`, {
+      const trackingData = await fetchWithTimeoutAndRetry(`https://api.goshippo.com/tracks/${trackingNumber}`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
       });
